d51215903693401d16ac6871b44e8278556385f2
Astrid-VW <astridvanwelden@hotmail.com>
2024-01-10 16:15:51 +0100

    Iteracion#2

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -8,3 +8,9 @@ number = "50"
 // 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
 let h = "5" , y = "10"
 console.log(myFavoriteHero,number,h,y)
+
+//Iteracion#2: Variables avanzadas
+// 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25. 
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = "10"
+console.log(character.age)
