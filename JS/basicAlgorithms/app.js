763c28465842a942496d62e9abe7bc7b372f2098
Astrid-VW <astridvanwelden@hotmail.com>
2024-01-10 15:54:12 +0100

    iteracion#1

---
+++ JS/basicAlgorithms/app.js
@@ -0,0 +1,13 @@
+//Iteracion #1: Variables
+// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+let myFavoriteHero
+myFavoriteHero = "Hulk"
+// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+let number
+number = "50"
+// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+let h = "5" , y = "10"
+console.log
+
+//Iteracion #2: Variables avanzadas
+// 
