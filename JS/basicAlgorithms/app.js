cbf5c78e84136eb233a93e725cd4915c3de2c5de
Astrid-VW <astridvanwelden@hotmail.com>
2024-01-15 17:30:35 +0100

    ~Iteracion#4.4

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -47,3 +47,73 @@ let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
+ globalBasePrice = 25000
+car1.finalPrice = car1.basePrice + globalBasePrice
+car2.finalPrice = car2.basePrice + globalBasePrice
+console.log(car1, car2)
+
+/* Iteración #3: Operadores**
+
+3.1 Multiplica 10 por 5 y muestra el resultado mediante console.*/
+
+let x = 5
+x *= 10
+console.log(x)
+
+/* 2.2 Divide 10 por 2 y muestra el resultado en un console.*/
+
+let d = 10
+d /= 2
+console.log(d)
+
+/* 3.3 Muestra mediante un console el resto de dividir 15 por 9.*/
+
+let r = 15
+r %= 9
+console.log(r)
+
+/* 3.4 Usa el correcto operador de asignación que resultará en o = 15,
+teniendo dos variables p = 10 y j = 5.*/
+
+let p = 10
+let j = 5
+let o = p + j
+console.log(o)
+
+/* 3.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.*/
+
+let c = 10
+let m = 5
+let i = c * m
+console.log(i)
+
+/**Iteración #4: Arrays**
+
+4.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.*/
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+let avenger1 = avengers[0];
+console.log(avenger1)
+
+//4.2 Cambia el primer elemento de avengers a "IRONMAN"//
+avengers.splice(0, 1, "IRONMAN");
+console.log(avengers)
+
+//4.3 console numero de elementos en el array usando la propiedad correcta de Array.//
+console.log("longitud array avengers", avengers.length)
+
+
+/* //!4.4 Añade 2 elementos al array: "Morty" y "Summer".
+Muestra en consola el último personaje del array*/
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+rickAndMortyCharacters.splice(4,5);
+console.log(rickAndMortyCharacters.splice)
+
+//! 4.6 Elimina el segundo elemento del array y muestra el array por consola.//
+/* const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.slice(1)//
+console.log(rickAndMortyCharacters.slice)*/
+
+
