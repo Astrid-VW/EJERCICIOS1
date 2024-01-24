f8edeb5d17021cb1b52dd3ed6f429a8a897a27b6
Astrid-VW <astridvanwelden@hotmail.com>
2024-01-24 20:59:22 +0100

    Iteracion1.1Events

---
+++ JS/events/app.js
@@ -0,0 +1,23 @@
+/*Iteración #1: Creando Events
+
+
+1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
+evento click que ejecute un console log con la información del evento del click*/
+
+const button = document.querySelector("#btnToClick");
+button.textContent = "Presióname!";
+button.addEventListener("click", (e) => {
+    console.log(e)
+});
+
+document.body.appendChild(button)
+
+/*1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.*/
+
+const focusElement = document.getElementsByClassName.focus;
+focusElement.addEventListener("focus", (e) =>{
+    console.log(input.value)
+});
+
+
+/*1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.*/
