b28f0f15d020d4ea239309e76e23638ae5443760
Astrid-VW <astridvanwelden@hotmail.com>
2024-02-06 17:41:41 +0100

    ChangePass&Update

--- backend/src/api/routes/User.routes.js
+++ backend/src/api/routes/User.routes.js
@@ -1,7 +1,25 @@
 const { upload } = require("../../middleware/files.middleware");
-const { registerLargo, registerMedio, registerWithRedirect, sendCode, resendCode, checkNewUser, login, autoLogin, changePassword, sendPassword } = require("../controllers/User.controllers");
+const { 
+    registerLargo, 
+    registerMedio, 
+    registerWithRedirect, 
+    sendCode, 
+    resendCode, 
+    checkNewUser, 
+    login, 
+    autoLogin, 
+    changePassword, 
+    sendPassword,
+    modifyPassword,
+    update,
+} = require("../controllers/User.controllers");
 const express = require("express");
+const User = require("../models/User.model");
+const { isAuth } = require("../../middleware/auth.middleware");
 const UserRoutes = express.Router();
+
+
+//!----------------------ENDPOINTS SIN AUTENTICACIÓN-----------------
 
 UserRoutes.post("/registerLargo", upload.single("image"), registerLargo);
 
@@ -21,5 +39,11 @@ UserRoutes.post("/autoLogin", autoLogin);
 
 UserRoutes.patch("/forgotPassword", changePassword);
 UserRoutes.patch("/sendPassword/:id", sendPassword);
+
+//!----------------------ENDPOINTS CON AUTENTICACIÓN-----------------
+
+UserRoutes.patch("/changepassword", [isAuth], modifyPassword);
+
+UserRoutes.patch("/update/update/",[isAuth], upload.single("image"), update)
 
 module.exports = UserRoutes;
